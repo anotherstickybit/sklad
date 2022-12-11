ae2475632950ebeeae836d64e64332b8b01f319a
maratsadykov <marats93@yandex.ru>
2022-12-11 19:23:07 +0300

    initial commit

---
+++ src/test/java/ru/puppetters/sklad/SkladApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.puppetters.sklad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkladApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
